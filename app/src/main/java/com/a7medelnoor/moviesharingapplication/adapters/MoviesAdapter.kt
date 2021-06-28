5731eec49a583864fe5769275b3836ba7626411b
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 00:07:49 +0200

    create movies adapter class

---
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/adapters/MoviesAdapter.kt
@@ -0,0 +1,65 @@
+package com.a7medelnoor.moviesharingapplication.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.a7medelnoor.moviesharingapplication.databinding.MovieItemCardBinding
+import com.a7medelnoor.moviesharingapplication.model.Movies
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+internal class MoviesAdapter(private var moviesList: List<Movies>)  :
+    RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+    private  var binding: MovieItemCardBinding? = null
+   // internal inner class MoviesViewHolder(itemCardBinding: MovieItemCardBinding): RecyclerView.ViewHolder(itemCardBinding.root)
+    internal inner class MoviesViewHolder(itemCardBinding: MovieItemCardBinding): RecyclerView.ViewHolder(itemCardBinding.root)
+    private val differCallback = object :
+        DiffUtil.ItemCallback<Movies>() {
+        override fun areItemsTheSame(oldItem: Movies, newItem: Movies): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movies, newItem: Movies): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        binding = MovieItemCardBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MoviesViewHolder(binding!!)
+    }
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        val currentMovie = differ.currentList[position]
+
+        holder.itemView.apply {
+
+
+            Glide.with(this)
+                .load(currentMovie.movieImage)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(binding?.moviesImageView!!)
+
+            binding?.moviesNameTextview?.text = currentMovie.movieName
+
+
+        }
+//            .setOnClickListener { mView ->
+//            val direction = MoviesFragmentDirections
+//                .actionMoviesFragmentToMoviesFragmentDetails()
+//            mView.findNavController().navigate(direction)
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+}
