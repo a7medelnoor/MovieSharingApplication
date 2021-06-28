57700f1917e90eb4a64d5062bc690f40f7490998
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 00:30:37 +0200

    add endpoint to get movie

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/MoviesApi.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/MoviesApi.kt
@@ -1,4 +1,16 @@
 package com.a7medelnoor.moviesharingapplication.data.network
 
+import com.a7medelnoor.moviesharingapplication.model.Movies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MoviesApi {
+
+    // get movies
+    @GET("users/ser143961235/videos")
+    fun getMovies(): Call<Movies>
+    // search movies
+    @GET("users/ser143961235/videos")
+    fun searchMovies(@Query("search") query: String?): Call<Movies>
 }
