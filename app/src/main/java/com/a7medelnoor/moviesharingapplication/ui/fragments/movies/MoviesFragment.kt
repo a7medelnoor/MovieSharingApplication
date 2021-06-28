945ede6c3d12d38190b725131348d4df4dfbb19a
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 00:08:59 +0200

    create movies fragment

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
@@ -5,19 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.ViewPager
 import com.a7medelnoor.moviesharingapplication.R
+import com.a7medelnoor.moviesharingapplication.adapters.MoviesAdapter
+import com.a7medelnoor.moviesharingapplication.databinding.FragmentMoviesBinding
+import com.a7medelnoor.moviesharingapplication.model.Movies
+import com.a7medelnoor.moviesharingapplication.ui.fragments.favorites.FavoritesFragment
+import com.google.android.material.tabs.TabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 
 
-class MoviesFragment : Fragment() {
+class MoviesFragment : Fragment(R.layout.fragment_movies) {
+    private var _binding: FragmentMoviesBinding? = null
+    private val binding get() = _binding!!
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+
+        _binding = FragmentMoviesBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//      setupTabBar()
+    }
+
+    private fun setupTabBar() {
+        val adapter = FragmentPagerItemAdapter(
+            childFragmentManager,
+            FragmentPagerItems.with(activity)
+                .add("GRID", MoviesFragment::class.java)
+                .add("LIST", FavoritesFragment::class.java)
+                .create()
+        )
+
+//        binding.viewpager.adapter = adapter
+//        binding.viewpagertab.setViewPager(binding.viewpager)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
