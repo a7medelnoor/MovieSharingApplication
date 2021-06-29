f2bd079022f7acab903d099c03af400cbab8a2e5
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 16:20:09 +0200

    remove un used code

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
@@ -7,75 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.a7medelnoor.moviesharingapplication.R
-import com.a7medelnoor.moviesharingapplication.adapters.ViewPagerAdapter
 import com.a7medelnoor.moviesharingapplication.databinding.FragmentMoviesBinding
 import com.a7medelnoor.moviesharingapplication.ui.fragments.favorites.FavoritesFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
-import kotlinx.android.synthetic.main.fragment_movies.*
-import kotlinx.android.synthetic.main.fragment_movies.view.*
 
 class MoviesFragment : Fragment(R.layout.fragment_movies) {
     private var _binding: FragmentMoviesBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        val fragmentAdapter = ViewPagerAdapter(requireFragmentManager())
-//        viewPager.adapter = gmfragmentAdapter
-//        tabLayout.setupWithViewPager(viewPager)
-
-
-//        var adapter = ViewPagerAdapter(requireFragmentManager(), lifecycle)
-//
-//        binding.viewPager.adapter = adapter
-//        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-//
-//            when (position) {
-//
-//                0 -> {
-//                    tab.text = "GRID"
-//                }
-//                1 -> {
-//                    tab.text = "LIST"
-//                }
-//            }
-//
-//
-//        }.attach()
-
-
-//        fragment = MoviesFragment()
-//        fragmentTransaction = requireFragmentManager()!!.beginTransaction()
-//        fragmentTransaction!!.replace(R.id.frameLayout, fragment!!)
-//        fragmentTransaction!!.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-//        fragmentTransaction!!.commit()
-//        binding.tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab) {
-//                // creating cases for fragment
-//                when (tab.position) {
-//                    0 -> fragment = MoviesFragment()
-//                    1 -> fragment = SearchMovieFragment()
-//                }
-//                val fm = childFragmentManager
-//                val ft = fm.beginTransaction()
-//                ft.replace(R.id.frameLayout, fragment!!)
-//                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-//                ft.commit()
-//            }
-//            override fun onTabUnselected(tab: TabLayout.Tab) {
-//
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab) {
-//
-//            }
-//        })
-
-    }
 
     fun TabLayout.setupWithViewPager(viewPager: ViewPager2, labels: List<String>) {
 
