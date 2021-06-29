9c5b8ce6a2fd220643c86abc84730d4c1a9d8a6a
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 16:15:11 +0200

    edit movies model param

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/model/Movies.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/model/Movies.kt
@@ -6,12 +6,16 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Movies(
-    @SerializedName("id")
-    val id: Int,
+    @SerializedName("user_id")
+    val user_id: Int,
+    @SerializedName("video_id")
+    val video_id: Int,
     @SerializedName("movie_image")
     val pictures: Int,
     @SerializedName("movie_name")
     val name: String?,
     @SerializedName("movie_description")
     val description: String?,
+    @SerializedName("query")
+    val query: String?
 ):Parcelable
