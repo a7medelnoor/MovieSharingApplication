0b144afaf8c848183a1db178087b6924665c1423
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-29 16:11:14 +0200

    add constants

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Constants.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Constants.kt
@@ -4,5 +4,14 @@ class Constants {
 
     companion object {
         const val BASE_URL ="https://api.vimeo.com/"
+        const val API_KEY = "605b3db65b98413596e2b1374623f527"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "per_page"
+        const val QUERY_API_KEY = "apiKey"
+
+        // ROM DATABASE
+        const val  MOVIE_DATABASE = "movies_database"
+        const val MOVIE_TABLE = "movies_table"
     }
 }
