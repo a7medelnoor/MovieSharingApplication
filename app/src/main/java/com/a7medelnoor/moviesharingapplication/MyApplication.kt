44c8613209e2da4ef49c08b7445aeed12b1be5b7
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 20:50:27 +0200

    add facebook fresco to our application

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/MyApplication.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/MyApplication.kt
@@ -1,8 +1,13 @@
 package com.a7medelnoor.moviesharingapplication
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
+    }
 }
