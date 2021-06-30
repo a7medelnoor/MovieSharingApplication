982f05b9c7c9c796d88f8681715899b199310ecb
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 20:52:17 +0200

    create movies repository and handle safe api call and implement getMovies fun

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/repository/MoviesRepository.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/repository/MoviesRepository.kt
@@ -1,7 +1,13 @@
 package com.a7medelnoor.moviesharingapplication.repository
 
+import com.a7medelnoor.moviesharingapplication.data.network.MoviesApi
+import com.a7medelnoor.moviesharingapplication.data.network.SafeApiCall
 import com.a7medelnoor.moviesharingapplication.model.Movies
+import javax.inject.Inject
 
-class MoviesRepository {
+class MoviesRepository @Inject constructor(
+    private val moviesApi: MoviesApi
+): SafeApiCall() {
+    suspend fun getMovies() = safeApiCall {moviesApi.getMovies() }
 
 }
