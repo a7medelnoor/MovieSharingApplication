bfbc3738534cef9175c8725175a4d7031a2bd4fd
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 15:18:43 +0200

    results data class that holds all our params

---
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/model/Results.kt
@@ -0,0 +1,19 @@
+package com.a7medelnoor.moviesharingapplication.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Results(
+    @SerializedName("movie_id")
+    val movie_id: String,
+    @SerializedName("movie_name")
+    val movie_name: String,
+    @SerializedName("movie_thumbnail")
+    val movie_thumbnail: String,
+    @SerializedName("movie_description")
+    val movie_description: String,
+    @SerializedName("movie_rating")
+    val movie_rating: Int,
+):Parcelable
