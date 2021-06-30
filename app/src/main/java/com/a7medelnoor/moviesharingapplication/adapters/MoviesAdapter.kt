af1d722309c345ccd0becb15744545ccac690e83
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 21:02:30 +0200

    change model class name to the gridRecyclerView item and it's params

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/adapters/MoviesAdapter.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/adapters/MoviesAdapter.kt
@@ -1,26 +1,31 @@
 package com.a7medelnoor.moviesharingapplication.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.a7medelnoor.moviesharingapplication.databinding.MovieItemCardBinding
+import com.a7medelnoor.moviesharingapplication.model.GridRecyclerViewItem
 import com.a7medelnoor.moviesharingapplication.model.Movies
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 internal class MoviesAdapter(private var moviesList: List<Movies>)  :
     RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
     private  var binding: MovieItemCardBinding? = null
-   // internal inner class MoviesViewHolder(itemCardBinding: MovieItemCardBinding): RecyclerView.ViewHolder(itemCardBinding.root)
     internal inner class MoviesViewHolder(itemCardBinding: MovieItemCardBinding): RecyclerView.ViewHolder(itemCardBinding.root)
     private val differCallback = object :
-        DiffUtil.ItemCallback<Movies>() {
-        override fun areItemsTheSame(oldItem: Movies, newItem: Movies): Boolean {
-            return oldItem.video_id == newItem.video_id
+        DiffUtil.ItemCallback<GridRecyclerViewItem.Movies>() {
+        override fun areItemsTheSame(oldItem: GridRecyclerViewItem.Movies, newItem: GridRecyclerViewItem.Movies): Boolean {
+            return oldItem.movie_id == newItem.movie_id
         }
 
-        override fun areContentsTheSame(oldItem: Movies, newItem: Movies): Boolean {
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(
+            oldItem: GridRecyclerViewItem.Movies,
+            newItem: GridRecyclerViewItem.Movies
+        ): Boolean {
             return oldItem == newItem
         }
     }
@@ -42,11 +47,11 @@ internal class MoviesAdapter(private var moviesList: List<Movies>)  :
 
 
             Glide.with(this)
-                .load(currentMovie.pictures)
+                .load(currentMovie.movie_thumbnail)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(binding?.moviesImageView!!)
 
-            binding?.moviesNameTextview?.text = currentMovie.name
+            binding?.moviesNameTextview?.text = currentMovie.movie_name
 
 
         }
