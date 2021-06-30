9d64db3f854260cbbeca463ff5615c2b83c42f9c
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 14:40:45 +0200

    setup tabs grid and list with view pager

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/MoviesFragment.kt
@@ -4,34 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.a7medelnoor.moviesharingapplication.R
+import com.a7medelnoor.moviesharingapplication.adapters.ViewPagerAdapter
 import com.a7medelnoor.moviesharingapplication.databinding.FragmentMoviesBinding
-import com.a7medelnoor.moviesharingapplication.ui.fragments.favorites.FavoritesFragment
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 
 class MoviesFragment : Fragment(R.layout.fragment_movies) {
     private var _binding: FragmentMoviesBinding? = null
     private val binding get() = _binding!!
 
-    fun TabLayout.setupWithViewPager(viewPager: ViewPager2, labels: List<String>) {
 
-        if (labels.size != viewPager.adapter?.itemCount)
-            throw Exception("The size of list and the tab count should be equal!")
-
-        TabLayoutMediator(this, viewPager,
-            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
-                tab.text = labels[position]
-            }).attach()
-    }
-
+    @Nullable
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        @NonNull inflater: LayoutInflater, @NonNull container: ViewGroup?,
+        @NonNull savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
 
@@ -44,18 +33,26 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
         return binding.root
     }
 
-    private fun setupTabBar() {
-        val adapter = FragmentPagerItemAdapter(
-            childFragmentManager,
-            FragmentPagerItems.with(activity)
-                .add("GRID", MoviesFragment::class.java)
-                .add("LIST", FavoritesFragment::class.java)
-                .create()
-        )
-//       viewPagerAdapter =ViewPagerAdapter(childFragmentManager)
-//        binding.registerViewpager.adapter = adapter
-//        binding.registerTabs.setupWithViewPager(binding.registerViewpager)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ViewPagerAdapter(requireActivity(), lifecycle)
+        binding.viewPager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+//            Log.d(TAG,"taaab"+tab)
+//            Log.d(TAG,"poooo"+position)
+            when (position) {
+                0 -> {
+                    tab.text = "GRID"
+
+                }
+                else -> {
+                    tab.text = "LIST"
+                }
+            }
+        }.attach()
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
