09123aa7f14c8c68a4ba3b1a30477a7e62400615
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 20:51:11 +0200

    getMovies function that return list of movies

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/MoviesApi.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/MoviesApi.kt
@@ -1,5 +1,6 @@
 package com.a7medelnoor.moviesharingapplication.data.network
 
+import com.a7medelnoor.moviesharingapplication.model.GridRecyclerViewItem
 import com.a7medelnoor.moviesharingapplication.model.Movies
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,9 +9,9 @@ import retrofit2.http.Query
 interface MoviesApi {
 
     // get movies
-    @GET("users/143961235/videos")
-    fun getMovies(): Call<Movies>
-    // search movies
-    @GET("query")
-    fun searchMovies(@Query("query") query: String?): Call<Movies>
+    @GET("movies")
+    fun getMovies(): List<GridRecyclerViewItem.Movies>
+//    // search movies
+//    @GET("query")
+//    fun searchMovies(@Query("query") query: String?): Call<Movies>
 }
