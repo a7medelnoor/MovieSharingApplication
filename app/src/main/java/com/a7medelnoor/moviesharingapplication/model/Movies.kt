d70a0c407340f8c0c28bbf146058331c63c2377d
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-06-30 15:19:54 +0200

    movies data class that return list of our results params

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/model/Movies.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/model/Movies.kt
@@ -4,18 +4,8 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+
 data class Movies(
-    @SerializedName("user_id")
-    val user_id: Int,
-    @SerializedName("video_id")
-    val video_id: Int,
-    @SerializedName("movie_image")
-    val pictures: Int,
-    @SerializedName("movie_name")
-    val name: String?,
-    @SerializedName("movie_description")
-    val description: String?,
-    @SerializedName("query")
-    val query: String?
-):Parcelable
+@SerializedName("results")
+internal val results: List<Results>
+)
