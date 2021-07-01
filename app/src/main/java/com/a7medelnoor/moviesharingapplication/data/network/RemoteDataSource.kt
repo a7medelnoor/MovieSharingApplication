c5187f845373cf2082a5c942cceb1fb780aca785
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 22:42:06 +0200

    init remote data source and inject moviesApi interface that holds our apis endpoint

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/RemoteDataSource.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/data/network/RemoteDataSource.kt
@@ -1,38 +1,14 @@
 package com.a7medelnoor.moviesharingapplication.data.network
 
-import com.a7medelnoor.moviesharingapplication.BuildConfig
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.a7medelnoor.moviesharingapplication.model.Movies
+import com.a7medelnoor.moviesharingapplication.model.Results
+import retrofit2.Response
 import javax.inject.Inject
 
-class RemoteDataSource @Inject constructor() {
-    companion object {
-        private const val BASE_URL = "https://60db71d5801dcb0017291156.mockapi.io/"
-    }
-    fun <MoviesApi> buildApi(
-        moviesApi: Class<com.a7medelnoor.moviesharingapplication.data.network.MoviesApi>
-    ): com.a7medelnoor.moviesharingapplication.data.network.MoviesApi {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(getRetrofitClient())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(moviesApi)
-    }
-    private fun getRetrofitClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                chain.proceed(chain.request().newBuilder().also {
-                    it.addHeader("Accept", "application/json")
-                }.build())
-            }.also { client ->
-                if (BuildConfig.DEBUG) {
-                    val logging = HttpLoggingInterceptor()
-                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-                    client.addInterceptor(logging)
-                }
-            }.build()
+class RemoteDataSource @Inject constructor(
+    private val moviesApi: MoviesApi
+) {
+    suspend fun getMovies(queries: Map<String, String>) :Response<Movies>{
+        return moviesApi.getMovies(queries)
     }
 }
