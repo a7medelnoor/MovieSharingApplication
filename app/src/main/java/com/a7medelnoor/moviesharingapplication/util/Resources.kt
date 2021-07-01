fad84c3bcd734f7a02d66c421a9100025e4aa33b
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 12:56:30 +0200

    add resource loading class with nothing

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Resources.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Resources.kt
@@ -3,11 +3,12 @@ package com.a7medelnoor.moviesharingapplication.util
 import okhttp3.ResponseBody
 
 sealed class Resources<out T>{
-    object LOADING: Resources<Nothing>()
     data class SUCCESS<out T>(val value: T): Resources<T>()
     data class ERROR(
        val isNetworkError: Boolean,
        val errorCode: Int?,
        val errorBody: ResponseBody?
     ) : Resources<Nothing>()
+    object LOADING: Resources<Nothing>()
+
 }
