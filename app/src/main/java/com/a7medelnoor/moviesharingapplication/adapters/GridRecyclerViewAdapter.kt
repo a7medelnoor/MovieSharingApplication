03e12d9c0b3df30572a3d63b6de0a31a8b05f25e
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 22:39:56 +0200

    init dapter onBindViewHolder and movieViewHolder

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/adapters/GridRecyclerViewAdapter.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/adapters/GridRecyclerViewAdapter.kt
@@ -2,56 +2,50 @@ package com.a7medelnoor.moviesharingapplication.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.a7medelnoor.moviesharingapplication.R
 import com.a7medelnoor.moviesharingapplication.databinding.ItemMovieBinding
-import com.a7medelnoor.moviesharingapplication.databinding.ItemPlaylist1TitleBinding
-import com.a7medelnoor.moviesharingapplication.model.GridRecyclerViewItem
+import com.a7medelnoor.moviesharingapplication.model.Movies
+import com.a7medelnoor.moviesharingapplication.model.Results
+import com.a7medelnoor.moviesharingapplication.util.MoviesDiffUtill
 
-class GridRecyclerViewAdapter : RecyclerView.Adapter<GridRecyclerViewHolder>() {
-    var items = listOf<GridRecyclerViewItem>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
+class GridRecyclerViewAdapter : RecyclerView.Adapter<GridRecyclerViewAdapter.MovieViewHolder>() {
+    var movies = emptyList<Results>()
+    class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movieBinding: Results){
+           binding.result = movieBinding
+          binding.executePendingBindings()
         }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridRecyclerViewHolder {
-        return when (viewType) {
-            R.layout.item_playlist1_title -> GridRecyclerViewHolder.TitleViewHolder(
-                ItemPlaylist1TitleBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-            R.layout.item_movie -> GridRecyclerViewHolder.MovieViewHolder(
-                ItemMovieBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-            else -> throw IllegalArgumentException("Invalid ViewType Provided")
-
+        companion object{
+            fun from(parent: ViewGroup): MovieViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemMovieBinding.inflate(layoutInflater, parent, false)
+                return MovieViewHolder(binding)
+            }
         }
     }
-
-    override fun onBindViewHolder(holder: GridRecyclerViewHolder, position: Int) {
-        when (holder) {
-            is GridRecyclerViewHolder.TitleViewHolder -> holder.bind(items[position] as GridRecyclerViewItem.Title)
-            is GridRecyclerViewHolder.MovieViewHolder -> holder.bind(items[position] as GridRecyclerViewItem.Movies)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder.from(parent)
     }
+
 
     override fun getItemCount(): Int {
-        return items.size
+        return movies.size
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return when (items[position]) {
-            is GridRecyclerViewItem.Title -> R.layout.item_playlist1_title
-            is GridRecyclerViewItem.Movies -> R.layout.item_movie
-        }
+
+    fun setData(newData: Movies) {
+        val recipesDiffUtil = MoviesDiffUtill(movies, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        movies = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val currentMovies = movies[position]
+           holder.bind(currentMovies)
+
     }
 
 }
