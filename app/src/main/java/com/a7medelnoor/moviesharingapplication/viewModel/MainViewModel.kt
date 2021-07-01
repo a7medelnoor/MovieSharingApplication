1616e908e70281545be7c227ce97cc45b2df3ff6
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 22:25:22 +0200

    main view model

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/viewModel/MainViewModel.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/viewModel/MainViewModel.kt
@@ -1,4 +1,15 @@
 package com.a7medelnoor.moviesharingapplication.viewModel
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.a7medelnoor.moviesharingapplication.util.Constants.Companion.QUERY_NUMBER
+
+class MainViewModel (
+    application: Application
+        ):AndroidViewModel(application) {
+    fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries[QUERY_NUMBER] = "5"
+        return queries
+    }
 }
