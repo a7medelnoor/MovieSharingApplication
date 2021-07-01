86d8f6178bb6e6124524f97864d0365b30c222b0
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 22:34:35 +0200

    change api url

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Constants.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/util/Constants.kt
@@ -3,11 +3,12 @@ package com.a7medelnoor.moviesharingapplication.util
 class Constants {
 
     companion object {
-        const val BASE_URL ="https://api.vimeo.com/"
+        const val BASE_URL ="https://ld4nhearvv.api.quickmocker.com/"
+//        const val BASE_URL ="https://60db71d5801dcb0017291156.mockapi.io/"
         const val API_KEY = "605b3db65b98413596e2b1374623f527"
 
         // API Query Keys
-        const val QUERY_NUMBER = "per_page"
+        const val QUERY_NUMBER = "1"
         const val QUERY_API_KEY = "apiKey"
 
         // ROM DATABASE
