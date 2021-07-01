ce65040225d38c278de50ac2245d367aab742639
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 13:17:47 +0200

    setup recycleView and get grid list using view Model and override onDestroy

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/grid/GridFragment.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/ui/fragments/movies/grid/GridFragment.kt
@@ -1,6 +1,7 @@
 package com.a7medelnoor.moviesharingapplication.ui.fragments.movies.grid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,9 +15,12 @@ import com.a7medelnoor.moviesharingapplication.ui.hide
 import com.a7medelnoor.moviesharingapplication.ui.show
 import com.a7medelnoor.moviesharingapplication.util.Resources
 import com.a7medelnoor.moviesharingapplication.viewModel.MoviesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
-
+@AndroidEntryPoint
 class GridFragment : Fragment(R.layout.fragment_grid) {
+    private  val TAG = "GridFragment"
     private var _binding :FragmentGridBinding? = null
     private val binding get() = _binding!!
     private val moviesViewModels by viewModels<MoviesViewModel> ()
@@ -37,24 +41,34 @@ class GridFragment : Fragment(R.layout.fragment_grid) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-         binding.recyclerView.apply {
-             setHasFixedSize(true)
-             layoutManager = LinearLayoutManager(context)
-             adapter=gridRecyclerViewAdapter
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            val layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,true)
+            recyclerView.layoutManager = layoutManager
+            adapter=gridRecyclerViewAdapter
          }
-        moviesViewModels.gridListItemLiveData.observe(viewLifecycleOwner){ result ->
-            when(result) {
-                is Resources.ERROR -> {
-                    binding.progressBar.show()
-                }
-                Resources.LOADING -> binding.progressBar.show()
-                is Resources.SUCCESS -> {
-                    binding.progressBar.hide()
-                    gridRecyclerViewAdapter.items = result.value
-                }
+        moviesViewModels.gridListItemLiveData.observe(viewLifecycleOwner, { result ->
+            if(result != null) {
+                Log.d(TAG, "onViewCreated: "+result)
             }
+            when(result) {
+               is Resources.ERROR -> {
+                   binding.progressBr.hide()
+               }
+             Resources.LOADING -> {binding.progressBr.show()}
+               is Resources.SUCCESS -> {
+                   binding.progressBr.hide()
+                   gridRecyclerViewAdapter.items = result.value
+                   Log.d(TAG, "onViewCreated: "+gridRecyclerViewAdapter.items)
+               }
+           }
 
-        }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
