b432d2f7e9e5e5dd650ea8eb281a3f38d81350f9
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-07-01 22:41:07 +0200

    movie repository inject remote data source

--- app/src/main/java/com/a7medelnoor/moviesharingapplication/repository/MoviesRepository.kt
+++ app/src/main/java/com/a7medelnoor/moviesharingapplication/repository/MoviesRepository.kt
@@ -1,13 +1,11 @@
 package com.a7medelnoor.moviesharingapplication.repository
 
-import com.a7medelnoor.moviesharingapplication.data.network.MoviesApi
-import com.a7medelnoor.moviesharingapplication.data.network.SafeApiCall
-import com.a7medelnoor.moviesharingapplication.model.Movies
+import com.a7medelnoor.moviesharingapplication.data.network.RemoteDataSource
 import javax.inject.Inject
 
 class MoviesRepository @Inject constructor(
-    private val moviesApi: MoviesApi
-): SafeApiCall() {
-    suspend fun getMovies() = safeApiCall {moviesApi.getMovies() }
+    remoteDataSource: RemoteDataSource
+) {
+    val remoteDataSource = remoteDataSource
 
 }
